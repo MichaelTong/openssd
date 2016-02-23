5b8e3db062d20ae7fc7fa3b4804e27873a81526f
MichaelTong <tonghao1992@gmail.com>
2016-02-23 11:11:54 -0600

    tweak

--- OpenSSD-1.1.0/installer/installer-linux.c
+++ OpenSSD-1.1.0/installer/installer-linux.c
@@ -594,7 +594,7 @@ int is_jasmine(const char *name)
 	}
   
 	if (ioctl(fd, HDIO_GET_IDENTITY, id) < 0) {
-		perror("%s: HDIO_GET_IDENTITY", name);
+		perror(strcat(name, ": HDIO_GET_IDENTITY"));
 		exit(1);
 	}
 
